f18903b812713ee49134c94de8498207b9bce335
gandarufu <gandalf.ba@gmail.com>
2025-05-03 21:00:33 +0200

    Setup project

---
+++ scripts/calculator.js
@@ -0,0 +1,5 @@
+const buttonNumbers = document.querySelectorAll(".num");
+const buttonFunctions = document.querySelectorAll(".func");
+const buttonEqual = document.querySelector(".equal");
+
+const inputArray = [];
